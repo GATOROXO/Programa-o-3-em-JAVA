f7c31ff3ef88ce6e61660d835a5e35ee695c1655
Joao Paulo Alves Campos <152233739+GATOROXO@users.noreply.github.com>
2025-10-25 17:49:13 -0300

    adicionando atividade 3 da aula 2

---
+++ ResQuestionarios/aula2/aluno.java
@@ -0,0 +1,41 @@
+package ResQuestionarios.aula2;
+
+import java.util.Iterator;
+import java.util.ArrayList;
+
+public class aluno{
+    private static int id=0;
+    private String nome;
+    private int rga;
+    private int turma;
+    public static void main(String[] args) {
+        ArrayList classe = new ArrayList();
+        aluno cAluno = new aluno();
+        aluno cAluno2 = new aluno("Cleber", 1234, 20241);
+        aluno cAluno3 = new aluno("João", 5556, 20251);
+        classe.add(cAluno);
+        classe.add(cAluno2);
+        classe.add(cAluno3);
+        
+        Iterator i = classe.iterator();
+        while(i.hasNext()){
+            aluno A = (aluno)i.next();
+            System.out.println("aluno " + A.id +"\nnome: " + A.nome + "\nrga: " + A.rga+"\nturma: " + A.turma + "\n\n");
+        }
+    }
+
+    // construtores
+    aluno(String unome, int urga, int uturma){
+        this.id++;
+        this.nome = unome;
+        this.rga = urga;
+        this.turma = uturma;
+    }
+
+    aluno(){
+        this.id++;
+        this.nome = "Sem nome";
+        this.rga = 0;
+        this.turma = 0;
+    }
+}
